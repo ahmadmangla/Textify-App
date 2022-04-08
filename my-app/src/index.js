663a04a3aa729e68e9cd38079b4e639881289532
adilmangla8 <adil.ahmad@ucp.edu.pk>
2022-04-09 02:43:26 +0500

    Done

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,25 +1,7 @@
 import react from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { books } from './bookdata';
-import Book from './book';
-
-function Booklist() {
-    return (
-        <div className='main'>
-            <section className='book-list'>
-                {books.map((book) => {
-                    return (
-                        <Book book={book}> </Book>
-                    )
-                })}
-
-            </section>
-        </div>
-    );
-}
+import App from './App';
 
 
 
-
-ReactDOM.render(<Booklist />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
