ab7f335e9093f8e0b62aee4a385dbe804086441c
adilmangla8 <adil.ahmad@ucp.edu.pk>
2022-04-10 03:43:55 +0500

    done

--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Navbar = (props) => {
     return (
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <nav className="navbar navbar-expand-md navbar-light bg-light">
             <div className="container">
                 <a className="navbar-brand" href="/">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
--- my-app/src/Components/TextForm.js
+++ my-app/src/Components/TextForm.js
@@ -10,12 +10,26 @@ const TextForm = (props) => {
     setText(text.toUpperCase());
   }
 
+  const handleLowClick = () => {
+    setText(text.toLowerCase());
+  }
+
+  const handleClearText = ()=>{
+    setText('');
+  }
+
+  const handleClipboard = () => {
+    navigator.clipboard.writeText(text)
+  }
   return (
     <div className="mb-3 my-3">
-      <h1>{props.heading}</h1>
+      <h2>{props.heading}</h2>
       <textarea className="form-control" id="my-form" onChange={handleOnChange} placeholder={text} value={text} rows="8"></textarea>
       <div className='float-end'>Total Characters: {text.replace(/ /g, "").length}</div>
       <button className='btn btn-primary my-3' onClick={handleUpClick}> Convert to UpperCase </button>
+      <button className='btn btn-primary mx-2' onClick={handleLowClick}> Convert to LowerCase </button>
+      <button className='btn btn-danger' onClick={handleClearText}> Clear Text </button>
+      <button className='btn btn-primary mx-2' onClick={handleClipboard}> Copy Text to Clipboard </button>
     </div>
   )
 }
