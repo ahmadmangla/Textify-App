fadc52c04874962817c8766084cd2ef29867e7c9
adilmangla8 <adil.ahmad@ucp.edu.pk>
2022-04-12 03:21:05 +0500

    color final

--- Textify-App-Main/my-app/src/App.js
+++ Textify-App-Main/my-app/src/App.js
@@ -43,22 +43,6 @@ const App = () => {
         //   document.body.style.color = "";
         // }
   }
-      // case 2:
-      //    day = "Tuesday";
-      //   break;
-      // case 3:
-      //   day = "Wednesday";
-      //   break;
-      // case 4:
-      //   day = "Thursday";
-      //   break;
-      // case 5:
-      //   day = "Friday";
-      //   break;
-      // case 6:
-      //   day = "Saturday";
-  
-
   
   return <>
     <Navbar title="Textifyy" mode={mode} toggleMode={toggleMode}/>
--- Textify-App-Main/my-app/src/Components/Navbar.js
+++ Textify-App-Main/my-app/src/Components/Navbar.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import PropTypes from 'prop-types'
 import './Navbar.css';
 
 const Navbar = (props) => {
+    const [theme, settheme] = useState(false);
+    const [text, settext] = useState("Show");
+
+    const toggleTheme = () => {
+        settheme(!theme);
+        // settext("Hide");
+    }
     return (
         <nav className={`navbar navbar-expand-md navbar-${props.mode} bg-${props.mode}`}>
             <div className="container">
@@ -20,24 +27,30 @@ const Navbar = (props) => {
                         </li>
                     </ul>
                 </div>
-
-                <div className='choose-theme-styles'>
-                    {/* <span  className='white-theme' onClick={() => props.toggleMode("light", "#b530b5")}></span> */}
-                    <span  className='purple-theme' onClick={() => props.toggleMode("purple", "#b530b5")}></span>
-                    <span className='dark-theme' onClick={() => props.toggleMode("orange", "#f5a059")}></span>
-                    <span className='blue-theme' onClick={() => props.toggleMode("blue", "#5b9cc1")}></span>
-                    <span className='black-theme' onClick={() => props.toggleMode("dark", "#363d44")}></span>
-                </div>
-
-                <div className={`form-check form-switch text-${props.mode=="light" ? "dark" : "white"}`} >
-                    <input className="form-check-input" onClick={()=> props.toggleMode("dark", "#1e1e1e")} type="checkbox" id="flexSwitchCheckDefault" />
-                        <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
-                </div>
+               {theme && <Themecolor {...props}/>}
+                {/* <div className={`form-check form-switch text-${props.mode == "light" ? "dark" : "white"}`} >
+                    <input className="form-check-input" onClick={() => props.toggleMode("dark", "#1e1e1e")} type="checkbox" id="flexSwitchCheckDefault" />
+                    <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+                </div> */}
+                <button class="btn btn-dark" onClick={toggleTheme}>{/*text==="Hide" ? "Hide" : "Show"*/}Theme Colors</button>
             </div>
 
 
         </nav>
     )
+}
+
+const Themecolor = (props) => {
+    return <>
+        <div className='choose-theme-styles'>
+            {/* <span  className='white-theme' onClick={() => props.toggleMode("light", "#b530b5")}></span> */}
+            <span className='purple-theme' onClick={() => props.toggleMode("purple", "#b530b5")}></span>
+            <span className='dark-theme' onClick={() => props.toggleMode("orange", "#f5a059")}></span>
+            <span className='blue-theme' onClick={() => props.toggleMode("blue", "#5b9cc1")}></span>
+            <span className='black-theme' onClick={() => props.toggleMode("dark", "#363d44")}></span>
+        </div>
+
+    </>
 }
 
 Navbar.defaultProps = {
