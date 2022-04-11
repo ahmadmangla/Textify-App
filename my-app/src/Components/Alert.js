1b63a7cf2167dce0bf2df8af2b14438af6ba9127
ahmadmangla <88089865+ahmadmangla@users.noreply.github.com>
2022-04-11 17:12:37 +0500

    Error fixed

--- my-app/src/Components/Alert.js
+++ my-app/src/Components/Alert.js
@@ -2,9 +2,8 @@ import React from 'react'
 
 const Alert = (props) => {
     return (
-         props.alert && <div class="alert alert-primary alert-dismissible fade show" role="alert">
+         props.alert && <div className="alert alert-primary alert-dismissible fade show" role="alert">
             <strong>{props.alert.type}</strong> : {props.alert.message}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     )
     
